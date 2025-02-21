532b247b5aecd9482df3a88d64760562bf8c9288
ewgerr <eewger@gmail.com>
2025-02-21 17:54:22 +0200

    fix view on different screens

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const container = document.getElementById('container');
 const resizeButton = document.getElementById('resize-button');
 const maxGridSize = 100; 
+
+
 function createGrid(size) {
     container.innerHTML = ''; 
-    const itemSize = 960 / size; 
+    const containerSize = container.clientWidth; 
+    const itemSize = containerSize / size; 
 
     for (let i = 0; i < size * size; i++) {
         const gridItem = document.createElement('div');
@@ -11,7 +14,7 @@ function createGrid(size) {
         gridItem.style.width = `${itemSize}px`;
         gridItem.style.height = `${itemSize}px`;
 
-
+        
         gridItem.addEventListener('mouseenter', () => {
             gridItem.classList.add('hovered');
         });
@@ -24,6 +27,7 @@ function createGrid(size) {
 function promptGridSize() {
     let size = parseInt(prompt('Введіть кількість квадратів на сторону (максимум 100):', 16));
 
+    
     if (isNaN(size) || size <= 0 || size > maxGridSize) {
         alert('Будь ласка, введіть число від 1 до 100.');
         return;
@@ -32,6 +36,14 @@ function promptGridSize() {
     createGrid(size); 
 }
 
+
 createGrid(16);
 
+
 resizeButton.addEventListener('click', promptGridSize);
+
+
+window.addEventListener('resize', () => {
+    const currentSize = Math.sqrt(container.children.length); 
+    createGrid(currentSize); 
+});
