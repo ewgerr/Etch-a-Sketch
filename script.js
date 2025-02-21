1bbc57c092a2621af31ebfbab387c961e09c2eee
ewgerr <eewger@gmail.com>
2025-02-21 17:29:48 +0200

    add moaus fill

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ function createGrid() {
     for (let i = 0; i < gridSize * gridSize; i++) {
         const gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
+
+        // Додаємо обробник події для наведення миші
+        gridItem.addEventListener('mouseenter', () => {
+            gridItem.classList.add('hovered');
+        });
+
         container.appendChild(gridItem);
     }
 }
